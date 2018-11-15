337bc8bd9ad62519cf2cd48a68fcce732f9e1540
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-11-15 10:45:57 +0100

    first Vue with bulma

---
+++ main.js
@@ -0,0 +1,16 @@
+Vue.component('message', {
+template:`
+<article class="message">
+  <div class="message-header">
+    {{title}}
+  </div>
+  <div class="message-body">
+    {{ body }}
+  </div>
+</article>
+`
+});
+
+new Vue({
+  el: "#root"
+});
