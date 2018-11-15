8db927a643eb4e0b61bad11b498e44df377d1277
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-11-15 11:46:10 +0100

    inline isVisible = false

--- main.js
+++ main.js
@@ -12,19 +12,13 @@ template:`
 <article class="message" v-show="isVisible">
   <div class="message-header">
     {{title}}
-    <button type="button" @click="hideModal">X</button>
+    <button type="button" @click="isVisible = false">X</button>
   </div>
   <div class="message-body">
     {{ body }}
   </div>
 </article>
-`,
-
-methods: {
-  hideModal() {
-    this.isVisible = false;
-  }
-}
+`
 
 });
 
