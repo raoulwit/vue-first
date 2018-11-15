9eefd0d0d99820225bfb6b2e74badb157f63c731
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-11-15 11:43:45 +0100

    wegclick toegevoegd

--- main.js
+++ main.js
@@ -2,16 +2,30 @@ Vue.component('message', {
 
 props: ['title','body'],
 
+data() {
+  return {
+    isVisible: true
+  };
+},
+
 template:`
-<article class="message">
+<article class="message" v-show="isVisible">
   <div class="message-header">
     {{title}}
+    <button type="button" @click="hideModal">X</button>
   </div>
   <div class="message-body">
     {{ body }}
   </div>
 </article>
-`
+`,
+
+methods: {
+  hideModal() {
+    this.isVisible = false;
+  }
+}
+
 });
 
 new Vue({
